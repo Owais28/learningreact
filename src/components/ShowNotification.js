dbb937645630e5f9a4c2d4bcb8e62928d62adc59
Owais28 <owaisathar2882@gmail.com>
2022-05-12 14:01:54 +0530

    jb

--- src/components/ShowNotification.js
+++ src/components/ShowNotification.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import {BsBellFill, BsFillBellSlashFill} from 'react-icons/bs'
 export default function ShowNotification(props) {
-    const isNotificationOn = props.isNotificationOn;
+  const [showNotification,setShowNotification] = React.useState(false)
+    const isNotificationOn = showNotification;
     let bell;
+    function handleClick(event){
+      setShowNotification(
+        (prevNotification) => !prevNotification
+      )
+    }
     if(isNotificationOn){
-        bell = <BsBellFill className='bell'/>
+        bell = <BsBellFill onClick={handleClick} className='bell'/>
     } else {
-        bell = <BsFillBellSlashFill className='bell'/>
+        bell = <BsFillBellSlashFill onClick={handleClick} className='bell'/>
     }
   return (
     <div>{
